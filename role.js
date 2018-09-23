18256683604a90fe10ea61d4b39a72e0d87b06ca
wii3 <42401274+pannila@users.noreply.github.com>
2018-09-23 22:45:50 +0200

    Update role.js

--- role.js
+++ role.js
@@ -18,7 +18,7 @@ if (roles.length !== reactions.length) throw "Roles list and reactions list are 
 function generateMessages(){
     var messages = [];
     messages.push(initialMessage);
-    for (let role of roles) messages.push(`React below to get the **"${role}"** role!`); //DONT CHANGE THIS
+    for (let role of roles) messages.push(`React below to get the **"${role}"** to verified`); //DONT CHANGE THIS
     return messages;
 }
 
